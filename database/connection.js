905af76c5f8b1f9c53f7b88d31f30a18cac94424
mazhuri aziz <mazhuri.aziz@erajaya.com>
2023-01-10 14:00:14 +0700

    init commit
    Signed-off-by: mazhuri aziz <mazhuri.aziz@erajaya.com>

---
+++ database/connection.js
@@ -0,0 +1,5 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres:YwZc5n0o8ig3sNvl2WTm@containers-us-west-92.railway.app:6193/railway')
+
+module.exports = sequelize
